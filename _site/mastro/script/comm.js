371f7491a8538cd6b8c74a2ae0fc65250c3e0d2c
qkrtnwls0127 <38414556+qkrtnwls0127@users.noreply.github.com>
2018-07-10 13:45:42 +0900

    Add files via upload

--- _site/mastro/script/comm.js
+++ _site/mastro/script/comm.js
@@ -1,36 +1,43 @@
-$(function(){
-  
-    var num=0;
-    $('.main_slide li').eq(num).children('.big').fadeIn('fast');
-    $('.main_slide li').eq(num).children('.small').fadeTo("slow",1);
-    
+$(function () {
 
-    setInterval(function(){
-        $('.main_slide li').eq(num).children('.big').fadeOut('fast');
-        $('.main_slide li').eq(num).children('.small').fadeTo("slow",0.5);
-        
-        
-        
-        if(num==3){
-            num=0;
-            $('.main_slide li').eq(num).children('.big').fadeIn('fast');
-            $('.main_slide li').eq(num).children('.small').fadeTo("slow",1);
-        }else{
-            num++;
-            $('.main_slide li').eq(num).children('.big').fadeIn('fast');
-            $('.main_slide li').eq(num).children('.small').fadeTo("slow",1);
-        }
-    },2000);
-  
-    //banner layer popup
-  $('.col3 a').click(function(){
+  //menu
+  $('.menu_depth1>li').mouseover(function () {
+    $(this).children('.menu_depth2').stop().slideDown();
+  })
+  $('.menu_depth1>li').mouseleave(function () {
+    $(this).children('.menu_depth2').stop().slideUp();
+  })
+
+
+
+  var num = 0;
+  $('.main_slide li').eq(num).children('.big').fadeIn('fast');
+  $('.main_slide li').eq(num).children('.small').fadeTo("slow", 1);
+
+
+  setInterval(function () {
+    $('.main_slide li').eq(num).children('.big').fadeOut('fast');
+    $('.main_slide li').eq(num).children('.small').fadeTo("slow", 0.5);
+
+
+
+    if (num == 3) {
+      num = 0;
+      $('.main_slide li').eq(num).children('.big').fadeIn('fast');
+      $('.main_slide li').eq(num).children('.small').fadeTo("slow", 1);
+    } else {
+      num++;
+      $('.main_slide li').eq(num).children('.big').fadeIn('fast');
+      $('.main_slide li').eq(num).children('.small').fadeTo("slow", 1);
+    }
+  }, 2000);
+
+  //banner layer popup
+  $('.col3 a').click(function () {
     $('#modal').addClass('on');
   })
-  $('#modal button').click(function(){
+  $('#modal button').click(function () {
     $('#modal').removeClass('on');
   })
-  
+
 })
-
-
-
